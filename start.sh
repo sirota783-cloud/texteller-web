4ed6e9e80603bc42c51c16e3ca4033927f2718cd
sirota783-cloud <sirota783@gmail.com>
2026-08-09 13:47:49 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 PORT="${PORT:-10000}"
-exec texteller launch --port "$PORT" --use-onnx --ngpu-per-replica 0
+RAY_memory_monitor_refresh_ms=0 exec texteller launch --port "$PORT" --use-onnx
