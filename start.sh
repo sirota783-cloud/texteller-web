121f00411a0f414f4daedb7d4ae98a7b7a0564b6
sirota783-cloud <sirota783@gmail.com>
2026-08-09 12:16:00 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 PORT="${PORT:-10000}"
-exec texteller web --server.address 0.0.0.0 --server.port "$PORT" --server.headless true
+exec texteller launch --host 0.0.0.0 --port "$PORT" --use-onnx
