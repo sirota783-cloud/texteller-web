2d58f1ee2171af1de2c56f659cfbf126e48813cc
sirota783-cloud <sirota783@gmail.com>
2026-08-13 01:13:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-exec gunicorn --bind "0.0.0.0:${PORT:-10000}" --workers 1 --threads 1 --timeout 180 app:app
+exec gunicorn --bind "0.0.0.0:${PORT:-10000}" --workers 1 --threads 1 --timeout 600 app:app
